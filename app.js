016003d0c3f117ad03373eb14bca241ed7619582
jam openlab <jb.cavarec@orange.fr>
2015-04-09 19:44:12 +0200

    ajout bodyparser+debug saisie remontees

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var util = require('util');
 var path = require("path");
 var express = require("express");
+var bodyParser = require("body-parser");
 var morgan = require("morgan");
 var serveStatic = require("serve-static");
 var _ = require('lodash');
@@ -19,10 +20,9 @@ app.set("views", path.join(__dirname,'.', "views"));
 app.set("view engine", "ejs");
 app.use(morgan("dev"));
 app.use(serveStatic(path.join(__dirname,'.', "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/bazar', function (req, res) {
- 
-
     var collection = db.get('bazar');
     collection.find({},{},function(e,docs){
       console.log(docs)
@@ -36,7 +36,9 @@ app.get('/remontee', function (req, res) {
  res.render('remontee')
 })
 
-app.post('/datas',function(req,res ){
-  console.log(req.params.datas)
+app.post('/datas',function(req,res){
+  console.log(req.body.saisie);
+  var collection = db.get('tri1')
+  collection.insert(JSON.parse(req.body.saisie))
 })
 
