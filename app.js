14c58657958199ffe485a71ac722a70fdedd1363
alex.diazrod@gmail.com <alex.diazrod@gmail.com>
2015-04-09 15:27:30 +0200

    version 1 de appli afpa

---
+++ app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var util = require('util');
+var path = require("path");
+var express = require("express");
+var morgan = require("morgan");
+var serveStatic = require("serve-static");
+var _ = require('lodash');
+var $ = require('jquery');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/afpa');
+
+var app = module.exports = express();
+
+app.enable("trust proxy");
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname,'.', "views"));
+app.set("view engine", "ejs");
+app.use(morgan("dev"));
+app.use(serveStatic(path.join(__dirname,'.', "public")));
+
+app.get('/bazar', function (req, res) {
+ 
+
+    var collection = db.get('bazar');
+    collection.find({},{},function(e,docs){
+      console.log(docs)
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+
+
+});
+/*
+app.get('/remontee', function (req, res) { 
+ res.render('remontee')
+})
+*/
---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var http = require("http");
+var app = require("./app");
+
+var server = module.exports = http.createServer(app);
+
+//require("./lib/websocket")(server);
+
+server.listen(app.get("port")).on("listening", function () {
+  console.log("server ready", this.address());
+});
